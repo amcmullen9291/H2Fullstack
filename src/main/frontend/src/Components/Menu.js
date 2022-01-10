c17bb1730e02e5818a50e396c75a9a02874be0c0
amcmullen9291 <amcmullen9291@gmail.com>
2022-01-10 14:07:04 -0500

    both tables populated

--- src/main/frontend/src/Components/Menu.js
+++ src/main/frontend/src/Components/Menu.js
@@ -1,27 +1,76 @@
 import { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Menu() {
 
 let  [ Cheeses, setCheeses ] = useState(null);
 let [ Beverages , setBeverages ] = useState(null);
 
-const cheeseList = fetch('http://localhost:8080/beverages/').then(resp => resp.json());
+const cheeseList = fetch('http://localhost:8080/cheeses/').then(resp => resp.json());
 const beverageList = fetch('http://localhost:8080/beverages/').then(resp => resp.json());
 
-console.log(cheeseList);
+
+useEffect(() => {
+    Promise.all([beverageList, cheeseList]).then(data => { setBeverages(data[0]); setCheeses(data[1]) })
+}, [])
 
 if((Cheeses) && Object.keys(Cheeses).length > 1){
+
 console.log("cheese list: ", Cheeses);
 }
 if((Beverages) && Object.keys(Beverages).length > 1){
 console.log("beverages list: ", Beverages);
 }
 
+function sortDrinks(e, cheeseName){
+    e.preventDefault();
+    console.log("Cheese: ", cheeseName);
+}
+
+function sortCheeses(e, beverageName){
+    e.preventDefault();
+    console.log("beverage: ", beverageName);
+}
+
 
 return (
         <>
         <div>
+         {Cheeses && (
+              <div><table id="cheeseList">
+                <thead>
+                </thead>
+                <tbody>
+                {Cheeses.map((cheese, id) => (
+                  <div key={id}>
+                    <tr>
+                    <td><button className="name" onClick={(e) => {sortDrinks(e, cheese.cheeseName)}}>{cheese.cheeseName}</button></td>
+                    </tr>
+                  </div>
+                ))}
+                </tbody>
+        </table>
+              </div>
+            )}
+        </div>
+        <div>
+        {Beverages && (
+                      <div><table id="beverageList">
+                        <thead>
+                        </thead>
+                        <tbody>
+                        {Beverages.map((beverage, id) => (
+                          <div key={id}>
+                            <tr>
+                            <td><button className="name" onClick={(e) => {sortCheeses(e, beverage.beverageName)}}>{beverage.beverageName}</button></td>
+                            </tr>
+                          </div>
+                        ))}
+                        </tbody>
+                </table>
+                      </div>
+                    )}
         </div>
         </>
     )
